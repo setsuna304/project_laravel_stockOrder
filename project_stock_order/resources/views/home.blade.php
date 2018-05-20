7a9bf3f48ae1779a000e66d4ec8844d6bed8dd4f
innovator <pondzza@gmail.com>
2018-05-20 23:28:14 +0700

    auth and popup complete login

---
+++ project_stock_order/resources/views/home.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        @endsection
+        @push('scripts')
+            <script>
+                $(document).ready(function () {
+                    swal("Login success", "welcom to stockorder", "success");
+                });
+
+            </script>
+
+    @endpush
